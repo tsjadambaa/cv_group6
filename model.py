9d24a2de1f18d04110018915d51896780bceacb1
karpovichalex <karpovehalex@gmail.com>
2023-11-19 17:10:40 +0300

    Start 5th lab

--- model.py
+++ model.py
@@ -1,53 +1,110 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import math
-# import pytorch_colors as colors
-import numpy as np
+import tensorflow as tf
 
 
-class enhance_net_nopool(nn.Module):
+def unet_model(image_size, output_classes):
 
-    def __init__(self):
-        super(enhance_net_nopool, self).__init__()
+    #Входной слой
+    input_layer = tf.keras.layers.Input(shape=image_size + (3,))
+    conv_1 = tf.keras.layers.Conv2D(64, 4, activation=tf.keras.layers.LeakyReLU(),
+                                    strides=2, padding='same', kernel_initializer='glorot_normal',
+                                    use_bias=False)(input_layer)
+    #Сворачиваем
+    conv_1_1 = tf.keras.layers.Conv2D(128, 4, activation=tf.keras.layers.LeakyReLU(), strides=2,
+                                      padding='same', kernel_initializer='glorot_normal',
+                                      use_bias=False)(conv_1)
+    batch_norm_1 = tf.keras.layers.BatchNormalization()(conv_1_1)
 
-        self.relu = nn.ReLU(inplace=True)
+    #2
+    conv_2 = tf.keras.layers.Conv2D(256, 4, activation=tf.keras.layers.LeakyReLU(), strides=2,
+                                      padding='same', kernel_initializer='glorot_normal',
+                                      use_bias=False)(batch_norm_1)
+    batch_norm_2 = tf.keras.layers.BatchNormalization()(conv_2)
 
-        number_f = 32
-        self.e_conv1 = nn.Conv2d(3, number_f, 3, 1, 1, bias=True)
-        self.e_conv2 = nn.Conv2d(number_f, number_f, 3, 1, 1, bias=True)
-        self.e_conv3 = nn.Conv2d(number_f, number_f, 3, 1, 1, bias=True)
-        self.e_conv4 = nn.Conv2d(number_f, number_f, 3, 1, 1, bias=True)
-        self.e_conv5 = nn.Conv2d(number_f * 2, number_f, 3, 1, 1, bias=True)
-        self.e_conv6 = nn.Conv2d(number_f * 2, number_f, 3, 1, 1, bias=True)
-        self.e_conv7 = nn.Conv2d(number_f * 2, 24, 3, 1, 1, bias=True)
+    #3
+    conv_3 = tf.keras.layers.Conv2D(512, 4, activation=tf.keras.layers.LeakyReLU(), strides=2,
+                                      padding='same', kernel_initializer='glorot_normal',
+                                      use_bias=False)(batch_norm_2)
+    batch_norm_3 = tf.keras.layers.BatchNormalization()(conv_3)
 
-        self.maxpool = nn.MaxPool2d(2, stride=2, return_indices=False, ceil_mode=False)
-        self.upsample = nn.UpsamplingBilinear2d(scale_factor=2)
+    #4
+    conv_4 = tf.keras.layers.Conv2D(512, 4, activation=tf.keras.layers.LeakyReLU(), strides=2,
+                                      padding='same', kernel_initializer='glorot_normal',
+                                      use_bias=False)(batch_norm_3)
+    batch_norm_4 = tf.keras.layers.BatchNormalization()(conv_4)
 
-    def forward(self, x):
-        x1 = self.relu(self.e_conv1(x))
-        # p1 = self.maxpool(x1)
-        x2 = self.relu(self.e_conv2(x1))
-        # p2 = self.maxpool(x2)
-        x3 = self.relu(self.e_conv3(x2))
-        # p3 = self.maxpool(x3)
-        x4 = self.relu(self.e_conv4(x3))
+    #5
+    conv_5 = tf.keras.layers.Conv2D(512, 4, activation=tf.keras.layers.LeakyReLU(), strides=2,
+                                      padding='same', kernel_initializer='glorot_normal',
+                                      use_bias=False)(batch_norm_4)
+    batch_norm_5 = tf.keras.layers.BatchNormalization()(conv_5)
 
-        x5 = self.relu(self.e_conv5(torch.cat([x3, x4], 1)))
-        # x5 = self.upsample(x5)
-        x6 = self.relu(self.e_conv6(torch.cat([x2, x5], 1)))
+    #6
+    conv_6 = tf.keras.layers.Conv2D(512, 4, activation=tf.keras.layers.LeakyReLU(), strides=2,
+                                      padding='same', kernel_initializer='glorot_normal',
+                                      use_bias=False)(batch_norm_5)
 
-        x_r = F.tanh(self.e_conv7(torch.cat([x1, x6], 1)))
-        r1, r2, r3, r4, r5, r6, r7, r8 = torch.split(x_r, 3, dim=1)
 
-        x = x + r1 * (torch.pow(x, 2) - x)
-        x = x + r2 * (torch.pow(x, 2) - x)
-        x = x + r3 * (torch.pow(x, 2) - x)
-        enhance_image_1 = x + r4 * (torch.pow(x, 2) - x)
-        x = enhance_image_1 + r5 * (torch.pow(enhance_image_1, 2) - enhance_image_1)
-        x = x + r6 * (torch.pow(x, 2) - x)
-        x = x + r7 * (torch.pow(x, 2) - x)
-        enhance_image = x + r8 * (torch.pow(x, 2) - x)
-        r = torch.cat([r1, r2, r3, r4, r5, r6, r7, r8], 1)
-        return enhance_image_1, enhance_image, r
+    #Разворачиваем
+    #1
+    up_1 = tf.keras.layers.Concatenate()([tf.keras.layers.Conv2DTranspose(512, 4, activation='relu', strides=2,
+                                                                          padding='same',
+                                                                          kernel_initializer='glorot_normal',
+                                                                          use_bias=False)(conv_6), conv_5])
+    batch_up_1 = tf.keras.layers.BatchNormalization()(up_1)
+
+    #Добавим Dropout от переобучения
+    batch_up_1 = tf.keras.layers.Dropout(0.25)(batch_up_1)
+
+    #2
+    up_2 = tf.keras.layers.Concatenate()([tf.keras.layers.Conv2DTranspose(512, 4, activation='relu', strides=2,
+                                                                          padding='same',
+                                                                          kernel_initializer='glorot_normal',
+                                                                          use_bias=False)(batch_up_1), conv_4])
+    batch_up_2 = tf.keras.layers.BatchNormalization()(up_2)
+    batch_up_2 = tf.keras.layers.Dropout(0.25)(batch_up_2)
+
+
+
+
+    #3
+    up_3 = tf.keras.layers.Concatenate()([tf.keras.layers.Conv2DTranspose(512, 4, activation='relu', strides=2,
+                                                                          padding='same',
+                                                                          kernel_initializer='glorot_normal',
+                                                                          use_bias=False)(batch_up_2), conv_3])
+    batch_up_3 = tf.keras.layers.BatchNormalization()(up_3)
+    batch_up_3 = tf.keras.layers.Dropout(0.25)(batch_up_3)
+
+
+
+
+    #4
+    up_4 = tf.keras.layers.Concatenate()([tf.keras.layers.Conv2DTranspose(256, 4, activation='relu', strides=2,
+                                                                          padding='same',
+                                                                          kernel_initializer='glorot_normal',
+                                                                          use_bias=False)(batch_up_3), conv_2])
+    batch_up_4 = tf.keras.layers.BatchNormalization()(up_4)
+
+
+    #5
+    up_5 = tf.keras.layers.Concatenate()([tf.keras.layers.Conv2DTranspose(128, 4, activation='relu', strides=2,
+                                                                          padding='same',
+                                                                          kernel_initializer='glorot_normal',
+                                                                          use_bias=False)(batch_up_4), conv_1_1])
+    batch_up_5 = tf.keras.layers.BatchNormalization()(up_5)
+
+
+    #6
+    up_6 = tf.keras.layers.Concatenate()([tf.keras.layers.Conv2DTranspose(64, 4, activation='relu', strides=2,
+                                                                          padding='same',
+                                                                          kernel_initializer='glorot_normal',
+                                                                          use_bias=False)(batch_up_5), conv_1])
+    batch_up_6 = tf.keras.layers.BatchNormalization()(up_6)
+
+
+    #Выходной слой
+    output_layer = tf.keras.layers.Conv2DTranspose(output_classes, 4, activation='sigmoid', strides=2,
+                                                   padding='same',
+                                                   kernel_initializer='glorot_normal')(batch_up_6)
+
+    model = tf.keras.Model(inputs=input_layer, outputs=output_layer)
+    return model
